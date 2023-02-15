7d137a94598764b6b7d62747ff9d83d9b5c3b1e7
Huy <huyhq101096@gmail.com>
2023-02-15 17:14:45 +0700

    update huy

---
+++ Housing/js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    // $('.service__slider__content').slick({
+    //     autoplay: false,
+    //     autoplaySpeed: 4000,
+    //     slidesToShow: 5,
+    //     dots: true,
+    //     infinity: true
+
+    // })
+
+
+    $('.counter').countUp();
+
+});
